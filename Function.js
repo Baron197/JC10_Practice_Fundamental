f45be7e30dc023f6e3133b7ac938aeac78f9fb4f
Baron Hartono <baronhartono@gmail.com>
2019-09-11 13:34:21 +0700

    commit

--- Function.js
+++ Function.js
@@ -37,19 +37,72 @@
 // var b = arr[5].hello()('test')
 // var c = arr[5].hello()('test')['kort'][1]
 
-var a = new Array(5)
-a[2] = 50
-console.log(a)
-console.log(a.length)
+// var a = new Array(5)
+// a[2] = 50
+// console.log(a)
+// console.log(a.length)
 
-var c = 'Test'
-console.log(c)
-console.log(typeof(c))
-console.log(c.split())
+// var c = 'Test'
+// console.log(c)
+// console.log(typeof(c))
+// console.log(c.split())
+// var angka = [1,2,3,4,5]
+
+// var hasil = angka.map((item) => {
+//     return [1,2]
+// })
+// console.log(hasil)
+// var hasil1 = angka.filter((item) => {
+//     return 'test'
+// })
+// console.log(hasil1)
 
 var map = (arr, fn) => {
-    return []
+    var newArr = []
+
+    for(var i = 0; i < arr.length; i++) {
+        newArr.push(fn(arr[i]))
+    }
+
+    return newArr;
 }
-var filter = (arr, fn) => {
-    return []
-}
+
+// var filter = (arr, fn) => {
+//     var newArr = []
+
+//     for(var i = 0; i < arr.length; i++) {
+//         var condition = fn(arr[i])
+//         if(condition) {
+//             newArr.push(arr[i])
+//         }
+//     }
+
+//     return newArr;
+// }
+
+// var numbers = [1,2,3,4,5]
+
+// var hasil = filter(numbers, (item) => {
+//     return item === 2
+// })
+// console.log(hasil)
+
+// var tampilkanScoreBoard = (angka) => {
+//     var arr = []
+
+//     while(angka > 0) {
+//         var angkaTerakhir = angka % 10;
+//         arr.unshift(angkaTerakhir)
+
+//         angka = Math.floor(angka / 10)
+//     } 
+
+//     return arr
+// }
+
+// var a = 3157
+
+// console.log(tampilkanScoreBoard(a))
+
+console.log('_\n_|\n_|')
+console.log(' _\n|_\n _|')
--- Looping.js
+++ Looping.js
@@ -3,30 +3,7 @@
 // console.log('Hello')
 // console.log('Hello')
 // console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
-// console.log('Hello')
+
 // var i = 1;
 // while(i <= 500) {
 //     console.log('Hello')
@@ -41,12 +18,23 @@
 //     }
 // }
 
+var bikinInterval = (fn, time) => {
+    for(var i = 1;; i++) {
+        if(i % (time * 100000) === 0) {
+            fn()
+        }
+    }
+}
 var bikinTimeout = (fn, time) => {
-    
+    for(var i = 1;i <= time * 1000000; i++) {
+    }
+    fn()
 }
 
 var contoh = () => {
     console.log('Hello')
 }
 
-bikinTimeout(contoh, 3000)
+bikinInterval(contoh, 3000)
+bikinTimeout(contoh,3000)
+// setInterval(contoh,2000)
